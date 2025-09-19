4d29be4b371d76a1926eb80bc054a571e203d39d
Deborah Lyra <deborahp.lyra@gmail.com>
2025-09-19 16:32:53 -0300

    changed color banner

--- src/components/general/Banner.tsx
+++ src/components/general/Banner.tsx
@@ -2,7 +2,7 @@ import profile from '../../assets/profile/lais_image_3.jpeg';
 
 export function Banner() {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-between h-auto md:h-[550px] bg-dusty-rose p-6 md:p-10 shadow-xl ">
+        <div className="flex flex-col md:flex-row items-center justify-between h-auto md:h-[550px] bg-light-pink p-6 md:p-10 shadow-xl ">
             <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
                 <h1 className="text-4xl md:text-5xl font-bold text-gray-600 mb-4">Laís Gomes</h1>
                 <h3 className="text-xl text-gray-500 mb-6">Descubra o poder de uma alimentação equilibrada e personalizada para você. Juntos, vamos construir hábitos saudáveis que transformarão sua vida.</h3>
