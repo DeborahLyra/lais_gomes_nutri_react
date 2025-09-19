2e280345518dfb37586bcab0c5fdcd7793842719
Deborah Lyra <deborahp.lyra@gmail.com>
2025-09-19 16:46:33 -0300

    added btns - banner

--- src/components/general/Banner.tsx
+++ src/components/general/Banner.tsx
@@ -2,10 +2,19 @@ import profile from '../../assets/profile/lais_image_3.jpeg';
 
 export function Banner() {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-between h-auto md:h-[550px] bg-light-pink p-6 md:p-10 shadow-xl ">
+        <div className="flex flex-col md:flex-row items-center justify-between h-auto md:h-[550px] bg--light-gray p-6 md:p-10">
             <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
-                <h1 className="text-4xl md:text-5xl font-bold text-gray-600 mb-4">Laís Gomes</h1>
-                <h3 className="text-xl text-gray-500 mb-6">Descubra o poder de uma alimentação equilibrada e personalizada para você. Juntos, vamos construir hábitos saudáveis que transformarão sua vida.</h3>
+                <h1 className="text-4xl md:text-5xl font-bold text-primary mb-4">Laís Gomes</h1>
+                <h3 className="text-md md:text-kl text-green-gray mb-6">Descubra o poder de uma alimentação equilibrada e personalizada para você. Juntos, vamos construir hábitos saudáveis que transformarão sua vida.</h3>
+            <div className='flex items-center gap-8'>
+                <button className='w-40 text-white bg-primary p-2 rounded-md cursor-pointer'>  
+                    Agendar consulta
+                </button>
+                <button className='w-40 text-primary border-4 border-primary p-2 rounded-md cursor-pointer'>  
+                    Saiba mais
+                </button>
+
+            </div>
             </div>
             <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
                 <div className="relative">
@@ -14,7 +23,7 @@ export function Banner() {
                         alt="Semíramis Profile" 
                         className="h-[300px] md:h-[500px] w-auto rounded-xl shadow-lg object-cover"
                     />
-                    <div className="absolute top-[-10px] left-[-10px] w-full h-full border-4 border-roseTea rounded-xl -z-10"></div>
+                    <div className="absolute top-[-10px] left-[-10px] w-full h-full border-4 border-roseTea rounded-xl -z-10 border-primary"></div>
                 </div>
             </div>
         </div>
