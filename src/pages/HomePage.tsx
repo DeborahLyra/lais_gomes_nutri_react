32d65cc40deb96662257af25847543609964f1c4
Deborah Lyra <deborahp.lyra@gmail.com>
2025-09-23 21:43:26 -0300

    added: whatsapp btn

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import { WhatsappLogoIcon } from '@phosphor-icons/react'
+import { motion } from 'framer-motion';
+
 import { AboutMe } from '../components/general/AboutMe'
 import { Banner } from '../components/general/Banner'
 import { Footer } from '../components/general/Footer'
@@ -12,6 +15,18 @@ export function HomePage() {
       <AboutMe />
       <MyServirces />
       <Footer />
+      <motion.a
+        href="https://wa.me/551199999999"
+        target="_blank"
+        rel="noreferrer"
+        animate={{ y: [0, -10, 0] }}
+        transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
+        className="fixed right-4 bottom-4 z-50 cursor-pointer"
+      >
+        <div className="w-14 h-14 rounded-full bg-green-500 flex items-center justify-center shadow-lg">
+          <WhatsappLogoIcon size={32} className="text-white" />
+        </div>
+      </motion.a>
     </>
   )
 }
